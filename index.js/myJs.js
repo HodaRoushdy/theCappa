7586813e1f1e8161906b98877379e73dda566f10
HodaRoushdy <hodaroshdy106@gmail.com>
2023-02-06 17:12:43 +0200

    first commit

---
+++ index.js/myJs.js
@@ -0,0 +1,25 @@
+// let home = document.getElementById("homeContent")
+
+$("#homeSection .carousel-item #home #homeContent .stars").fadeIn(500,function(){
+    $("#homeSection .carousel-item #home #homeContent .content").fadeIn(500,function(){
+        // $("#homeSection #home #homeContent .content h1").fadeIn(500,function(){
+        //     $("#homeSection #home #homeContent .content button").fadeIn(500)
+        // })
+    })
+});
+$(".firststars").slideDown(500,function(){
+    $(".secondContent").slideDown(500,function(){
+        $(".divIcone").slideDown(500 , function(){
+            $(".firstImg").slideDown(500,function(){
+                $(".secondImg").slideDown(500,function(){
+                })
+            })
+        })
+    })
+
+})
+$(".sixthContent").fadeIn(1000,function(){
+        $(".first3Items").slideDown(500 , function(){
+                    $(".second3Items").slideDown(500)
+                    })
+            })
